b581f9c76848738417f46439e7fd15430902966d
Alexander1Konstantinov <91659786+Alexander1Konstantinov@users.noreply.github.com>
2021-10-21 23:35:48 +0400

    Add files via upload

---
+++ dz №3/ZZadanie1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+int f(int n)
+{
+    if (n / 10 > 0)
+        f(n / 10);
+    cout << n%10 << endl;
+    return 0;
+}
+int main()
+{
+    int n;
+    cin >> n;
+    f(n);
+}
---
+++ dz №3/Zzadanie 3.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int f(int a, int b)
+{
+	if (a == 1)
+		return 1;
+	while (1)
+	{
+		if (a % b == 0)
+		{
+			cout << b << endl;
+			f(a / b, b);
+			return 2;
+		}
+		b++;
+	}
+}
+int main()
+{
+	int a;
+	cin >> a;
+	f(a, 2);
+}
