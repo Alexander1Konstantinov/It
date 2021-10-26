bcbd9bf3999fdfd25223b96ebff0fc464b270cb9
Alexander1Konstantinov <91659786+Alexander1Konstantinov@users.noreply.github.com>
2021-10-26 21:44:54 +0400

    Add files via upload

---
+++ dz №4/ZZZadanie1.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+
+void print_array(int mas[], const int n)
+{
+	for (int i=0; i < n; i++)
+		cout << mas[i]<<' ';
+	cout << endl;
+}
+
+void enarray(int first, int dif, const int n, int mas[])
+{
+	for (int i = 0; i < n; i++)
+	{
+		mas[i] = first;
+		first += dif;
+	}
+}
+
+void rand_array1(int mas[], int n)
+{
+	srand(time(0));
+	for (int i = 0; i < n; i++)
+		mas[i] = rand() % n + 1;
+}
+
+void rand_array2(int mas[], int n)
+{
+	srand(time(0));
+	for (int i = 0; i < n; i++)
+		mas[i] = rand() % 1001 + 1000;
+}
+
+void count(int mas[], int n)
+{
+	int k = 0;
+	for (int i = 0; i < n; i++)
+		if (((mas[i] % 100) / 10) % 2 == 0)
+			k++;
+	cout<< k<<endl;
+}
+
+int main()
+{
+	int first, dif;
+	int n;
+	cin >> first >> dif >> n;
+	int* mas = new int[n];
+	enarray(first, dif, n, mas);
+	print_array(mas, n);
+	rand_array1(mas, n);
+	print_array(mas, n);
+	rand_array2(mas, n);
+	count(mas, n);
+	print_array(mas, n);
+}
