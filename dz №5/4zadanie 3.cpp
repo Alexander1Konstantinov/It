0d91eb67dedb76856b701c3af42a25deaa1ba3ce
Alexander1Konstantinov <91659786+Alexander1Konstantinov@users.noreply.github.com>
2021-12-15 00:34:54 +0400

    Add files via upload

--- dz №5/4zadanie 3.cpp
+++ dz №5/4zadanie 3.cpp
@@ -3,6 +3,15 @@
 
 using namespace std;
 
+void vivod(char* a, char* b, char* c)
+{
+	cout << b[0] << '.' << c[0] << '.' <<' '<< a<<endl;
+}
+void delete_char(char* a)
+{
+	delete[] a;
+}
+
 int main()
 {
 	SetConsoleCP(1251);
@@ -11,8 +20,8 @@ int main()
 	char* b = new char[100];
 	char* c = new char[100];
 	cin >> a >> b >> c;
-	cout << b[0]<<'.'<<c[0]<<'.'<<a;
-	delete[] a;
-	delete[] b;
-	delete[] c;
+	vivod(a, b, c);
+	delete_char(a);
+	delete_char(b);
+	delete_char(c);
 }
